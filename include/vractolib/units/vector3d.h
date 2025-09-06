a4e2c26f53bbafe582dedf0610b969f21c37a3f6
vracton <sonit.sahoo@icloud.com>
2025-09-06 11:41:09 -0500

    add vector3d

---
+++ include/vractolib/units/vector3d.h
@@ -0,0 +1,34 @@
+#ifndef VECTOR3D_H
+#define VECTOR3D_H
+
+namespace vunits {
+    template<typename T> class Vector3D {
+        public:
+            T x, y, z;
+
+            Vector3D() : x(0.0), y(0.0), z(0.0) {}
+            Vector3D(T x, T y, T z) : x(x), y(y), z(z) {}
+
+            Vector3D operator+(const Vector3D<T> &v1) const {
+                return Vector3D<T>(x+v1.x, y+v1.y, z+v1.z);
+            }
+
+            Vector3D operator-(const Vector3D<T> &v1) const {
+                return Vector3D<T>(x-v1.x, y-v1.y, z-v1.z);
+            }
+
+            Vector3D operator*(const T &s) const {
+                return Vector3D<T>(s*x, s*y, s*z);
+            }
+
+            Vector3D operator/(const T &s) const {
+                return Vector3D<T>(x/s, y/s, z/s);
+            }
+
+            Vector3D operator*(const Vector3D<T> &v1) const {
+                return Vector3D<T>(x*v1.x, y*v1.y, z*v1.z);
+            }
+    };
+}
+
+#endif
